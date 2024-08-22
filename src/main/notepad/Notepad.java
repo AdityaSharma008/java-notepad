a1d02038144055c7423102b32f93b03bd659378d
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-08-22 23:36:31 +0530

    Change project structure and add buttons for edit menu
    - move notepad to src/main
    - write initial logic for edit options

--- src/main/notepad/Notepad.java
+++ src/main/notepad/Notepad.java
@@ -1,6 +1,8 @@
 package notepad;
 
 import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import java.awt.event.ActionEvent;
@@ -38,35 +40,54 @@ public class Notepad implements ActionListener {
             @Override
             public void insertUpdate(DocumentEvent e) {
                 saved = false;
+                editSelectAll.setEnabled(textPane.getText().length() > 0);
                 frame.setTitle("*" + notepadTitle);
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
                 saved = false;
+                editSelectAll.setEnabled(textPane.getText().length() > 0);
                 frame.setTitle("*" + notepadTitle);
             }
 
             @Override
             public void changedUpdate(DocumentEvent e) {
                 saved = false;
+                editSelectAll.setEnabled(textPane.getText().length() > 0);
                 frame.setTitle("*" + notepadTitle);
             }
         });
+
+        textPane.addCaretListener(new CaretListener() {
+            @Override
+            public void caretUpdate(CaretEvent e) {
+                editCut.setEnabled(e.getMark() - e.getDot() != 0);
+                editCopy.setEnabled(e.getMark() - e.getDot() != 0);
+                editDelete.setEnabled(e.getMark() - e.getDot() != 0);
+            }
+        });
+
         frame.add(scrollPane);
     }
 
     private void makeMenuBar(){
         menuBar = new JMenuBar();
+
         file = new JMenu("File");
         makeFileMenu();
+
         edit = new JMenu("Edit");
         makeEditMenu();
+
         format = new JMenu("Format");
         makeFormatMenu();
+
         view = new JMenu("View");
         makeViewMenu();
+
         menuBar.add(file); menuBar.add(edit); menuBar.add(format); menuBar.add(view);
+
         frame.setJMenuBar(menuBar);
     }
 
@@ -96,13 +117,45 @@ public class Notepad implements ActionListener {
 
     private void makeEditMenu(){
         editCut = new JMenuItem("Cut");
+        editCut.setEnabled(false);
+        editCut.addActionListener(this);
+        editCut.setActionCommand("Cut");
+
         editCopy = new JMenuItem("Copy");
+        editCopy.setEnabled(false);
+        editCopy.addActionListener(this);
+        editCopy.setActionCommand("Copy");
+
         editPaste = new JMenuItem("Paste");
+        editPaste.setEnabled(false);
+        editPaste.addActionListener(this);
+        editPaste.setActionCommand("Paste");
+
         editDelete = new JMenuItem("Delete");
+        editDelete.setEnabled(false);
+        editDelete.addActionListener(this);
+        editDelete.setActionCommand("Delete");
+
         editFind = new JMenuItem("Find");
+        editFind.setEnabled(false);
+        editFind.addActionListener(this);
+        editFind.setActionCommand("Find");
+
         editReplace = new JMenuItem("Replace");
+        editReplace.setEnabled(false);
+        editReplace.addActionListener(this);
+        editReplace.setActionCommand("Replace");
+
         editGoto = new JMenuItem("Goto");
+        editGoto.setEnabled(false);
+        editGoto.addActionListener(this);
+        editGoto.setActionCommand("Goto");
+
         editSelectAll = new JMenuItem("Select All");
+        editSelectAll.setEnabled(false);
+        editSelectAll.addActionListener(this);
+        editSelectAll.setActionCommand("SelectAll");
+
         edit.add(editCut); edit.add(editCopy); edit.add(editPaste); edit.add(editDelete); edit.add(editFind); edit.add(editReplace); edit.add(editGoto); edit.add(editSelectAll);
     }
 
