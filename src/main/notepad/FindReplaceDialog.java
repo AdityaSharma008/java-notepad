a67c203a910948ddf5fee9aeff1c0e45386b28b2
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2024-08-23 19:12:01 +0530

    Add find & replace functionality

--- src/main/notepad/FindReplaceDialog.java
+++ src/main/notepad/FindReplaceDialog.java
@@ -1,6 +1,8 @@
 package notepad;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,11 +12,14 @@ public class FindReplaceDialog implements ActionListener {
     JDialog frDialog;
     JButton btnCancel, btnFindNext, btnReplace, btnReplaceAll;
     JTextField txtFind, txtReplace;
+    int startIndex, oldIndex;
+    String text;
 
     FindReplaceDialog(Notepad notepad){
         this.notepad = notepad;
         this.frDialog = new JDialog(notepad.frame);
-
+        text = notepad.textPane.getText();
+        oldIndex = 0;
         makeDialogBox();
     }
 
@@ -26,10 +31,32 @@ public class FindReplaceDialog implements ActionListener {
 
         JLabel lblFind = new JLabel("Find: ");
         JLabel lblReplace = new JLabel("Replace with: ");
+
         txtFind = new JTextField(20);
         txtReplace = new JTextField(20);
 
+        DocumentListener docListener = new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                updateButtonStates();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                updateButtonStates();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                updateButtonStates();
+            }
+        };
+
+        txtFind.getDocument().addDocumentListener(docListener);
+        txtReplace.getDocument().addDocumentListener(docListener);
+
         btnFindNext = new JButton("Find Next");
+        btnFindNext.setEnabled(false);
         btnFindNext.addActionListener(this);
         btnFindNext.setActionCommand("FindNext");
 
@@ -38,11 +65,13 @@ public class FindReplaceDialog implements ActionListener {
         btnCancel.setActionCommand("Cancel");
 
         btnReplace = new JButton("Replace");
+        btnReplace.setEnabled(false);
         btnReplace.addActionListener(this);
         btnReplace.setActionCommand("Replace");
 
         btnReplaceAll = new JButton("Replace All");
-        btnReplace.addActionListener(this);
+        btnReplaceAll.setEnabled(false);
+        btnReplaceAll.addActionListener(this);
         btnReplaceAll.setActionCommand("ReplaceAll");
 
         gbc.insets = new Insets(5, 5, 5, 5);
@@ -85,21 +114,44 @@ public class FindReplaceDialog implements ActionListener {
         frDialog.setVisible(true);
     }
 
+    private void updateButtonStates() {
+        String replaceText = txtReplace.getText();
+        String findText = txtFind.getText();
+        boolean isFindTextNotEmpty = findText.length() > 0;
+        boolean isReplaceTextNotEmpty = replaceText.length() > 0;
+
+        btnFindNext.setEnabled(isFindTextNotEmpty);
+        btnReplace.setEnabled(isFindTextNotEmpty && isReplaceTextNotEmpty);
+        btnReplaceAll.setEnabled(isFindTextNotEmpty && isReplaceTextNotEmpty);
+    }
+
+    private void find(String toFind){
+        startIndex = text.indexOf(toFind, oldIndex);
+        oldIndex = startIndex + 1;
+        notepad.textPane.select(startIndex, startIndex + toFind.length());
+    }
+
     public void actionPerformed(ActionEvent e){
         String cmd = e.getActionCommand();
+        String toFind = txtFind.getText();
+        String newString = txtReplace.getText();
 
         switch (cmd) {
-            case "Cancel" -> {
-                frDialog.dispose();
-            }
-            case "FindNext" -> {
-                System.out.println("I am find next");
-            }
+            case "FindNext" -> find(toFind);
+            case "Cancel" -> frDialog.dispose();
             case "Replace" -> {
-                System.out.println("I am replace");
+                if(notepad.textPane.getSelectedText() == null){
+                    find(toFind);
+                    return;
+                }
+                else {
+                    notepad.textPane.replaceSelection(newString);
+                }
+                text = notepad.textPane.getText();
             }
             case "ReplaceAll" -> {
-                System.out.println("I am replace all");
+                notepad.textPane.setText(text.replaceAll(toFind, newString));
+                text = notepad.textPane.getText();
             }
         }
     }
